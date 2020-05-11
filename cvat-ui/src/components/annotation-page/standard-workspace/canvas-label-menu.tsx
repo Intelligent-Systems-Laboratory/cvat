c005dffbe347bc73676866bfd65d2d698c225754
jeffersonkeh <jeffersonkeh@gmail.com>
2020-05-11 11:27:40 +0800

    Add basic layout for label menu

--- cvat-ui/src/components/annotation-page/standard-workspace/canvas-label-menu.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/canvas-label-menu.tsx
@@ -6,7 +6,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
+import { Row, Col } from 'antd/lib/grid';
+import Icon from 'antd/lib/icon';
+import Select from 'antd/lib/select';
+import Radio, { RadioChangeEvent } from 'antd/lib/radio';
+import Checkbox, { CheckboxChangeEvent } from 'antd/lib/checkbox';
+import Divider from 'antd/lib/divider'
+import Input from 'antd/lib/input';
+import InputNumber from 'antd/lib/input-number';
+import Collapse from 'antd/lib/collapse';
+import Dropdown from 'antd/lib/dropdown';
+import Menu from 'antd/lib/menu';
 import Button from 'antd/lib/button';
+import Modal from 'antd/lib/modal';
+import Popover from 'antd/lib/popover';
+import Text from 'antd/lib/typography/Text';
+import Tooltip from 'antd/lib/tooltip';
+
+
 import ObjectItemContainer from 'containers/annotation-page/standard-workspace/objects-side-bar/object-item';
 
 interface Props {
@@ -55,14 +73,43 @@ export default function CanvasLabelMenu(props: Props): JSX.Element | null {
         top: top,
         left: left,
         backgroundColor: 'white',
+        padding: 5,
       };
+
+
+    console.log(objectID, objectState, attributes, labels);
 
     return ReactDOM.createPortal(
         <div className='cvat-canvas-label-menu' style={ divStyle }>
-            <Button type='link' icon='delete'>
+
+
+            {/* <Button type='link' icon='tags'>
                 Label Menu
-            </Button>
-            {/* <ObjectItemContainer clientID={activatedStateID} /> */}
+            </Button> */}
+            <Row type='flex' align='middle' justify='space-around'>
+                <Col span={12}>
+                    <Text style={{ fontSize:14 }}>Label</Text>
+                </Col>
+                <Col span={12}>
+                    <Text strong style={{ fontSize:16 }}>{objectState.label.name}</Text>
+                </Col>
+                {/* <Text style={{ fontSize: 16 }}>Label {objectState.label.name}</Text> */}
+            </Row>
+            {!!attributes.length && <Divider />}
+
+            {!!attributes.length
+                && attributes.map((attribute: any): JSX.Element => (
+                    <Row>
+                        <Col span={12}>
+                            <Text style={{ fontSize:14 }}>{attribute.name}</Text>
+                        </Col>
+                        <Col span={12}>
+                            <Text strong style={{ fontSize:16 }}>{objectState.attributes[attribute.id]}</Text>
+                        </Col>
+                    </Row>
+                ))
+                }
+            <ObjectItemContainer clientID={activatedStateID} />
         </div>,
         window.document.body,
     );

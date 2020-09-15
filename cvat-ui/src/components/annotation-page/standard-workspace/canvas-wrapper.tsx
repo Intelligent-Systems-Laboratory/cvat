ee8a1071e6eac61a1ec89f8d67f302aafc6de1eb
elcie3 <maverick_rivera@dlsu.edu.ph>
2020-09-15 12:51:49 +0800

    Fix bug that prevents changing frame after tracking

--- cvat-ui/src/components/annotation-page/standard-workspace/canvas-wrapper.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/canvas-wrapper.tsx
@@ -351,16 +351,15 @@ export default class CanvasWrapperComponent extends React.PureComponent<Props> {
                 let index = ((frameData.number - automaticTracking.frameStart)/2) - 1;
                 if(index<automaticTracking.states.length-1){
                     this.changeFrame(frameData.number+2);
+                }else{
+                    onSwitchAutoTrack(false);
                 }
                 if(frameData.number!== prevProps.frameData.number && automaticTracking.tracking){
                     const [state] = annotations.filter((el: any) => (el.clientID === automaticTracking.clientID));
                     // console.log(state);
                     // console.log('states',automaticTracking.states);
-                    console.log('index',index);
+                    // console.log('index',index);
                     try{
-                        if(index>=automaticTracking.states.length){
-                            onSwitchAutoTrack(false);
-                        }
                         let temp = automaticTracking.states[index];
                         // console.log(temp);
                         state.points = temp;
